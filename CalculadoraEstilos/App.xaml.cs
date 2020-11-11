3ac9e366e6504623016cb4738d30f5fb358a5114
SusanRojas <sf.rojas16@iesdoctorbalmis.com>
2020-11-11 01:10:31 +0100

    Agregar archivos de proyecto.

---
+++ CalculadoraEstilos/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculadoraEstilos
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            (MainWindow as MainWindow).ResultadoTextBox.Text += (sender as Button).Tag.ToString();
+        }
+    }
+}
